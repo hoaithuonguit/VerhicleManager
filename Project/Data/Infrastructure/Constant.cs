58a24e44ce046dca978cbbb14200ed1a45e88a4f
DESKTOP-FGNG84R\hoai thuong <hoaithuong.uit@gmail.com>
2018-05-27 23:01:28 +0700

    init pr

---
+++ Project/Data/Infrastructure/Constant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Infrastructure
+{
+    public class Constant
+    {
+        // DATE FORMAT
+        public static string FORMAT_DATETIME { get { return "dd/MM/yyyy hh:MM"; } }
+        public static string FORMAT_DATE { get { return "dd/MM/yyyy"; } }
+    }
+}
