08bb949b0d6f2c599f2d27f0f6e61b49ee2710f8
DESKTOP-FGNG84R\hoai thuong <hoaithuong.uit@gmail.com>
2018-06-03 23:39:08 +0700

    create index page

---
+++ Project/Services/Common.cs
@@ -0,0 +1,24 @@
+﻿using Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class Common : ICommon
+    {
+        public string ChangePathImage(string _path)
+        {
+            int index = _path.IndexOf("Image");
+            string path = "";
+            if (index != -1)
+            {
+                path = "\\" + _path.Substring(index);
+               path = path.Replace("\\", "/");
+            }
+            return path;
+        }
+    }
+}
--- Project/website/Controllers/HomeController.cs
+++ Project/website/Controllers/HomeController.cs
@@ -11,18 +11,28 @@ namespace website.Controllers
     public class HomeController : Controller
     {
         #region contructor
-        private ProductService service = new ProductService();
-       
+        private ProductService _service = new ProductService();
+        private Common _common = new Common();
         #endregion
 
         public ActionResult Index()
         {
             List<ProductDto> Verhicle = new List<ProductDto>();
-            Verhicle = service.GetAllProduct("01");
+            Verhicle = _service.GetAllProduct("01");
             List<ProductDto> Category = new List<ProductDto>();
-            Category = service.GetTopCategory(10);
+            Category = _service.GetAllProduct("02");
+            foreach (var item in Verhicle)
+            {
+                item.Image1 = _common.ChangePathImage(item.Image1);
+            }
+            foreach (var item in Category)
+            {
+                item.Image1 = _common.ChangePathImage(item.Image1);
+            }
             ViewBag.Verhicle = Verhicle;
+            ViewBag.VerhicleCount = Verhicle.Count();
             ViewBag.Category = Category;
+            ViewBag.CategoryCount = Category.Count();
             return View(ViewBag);
         }      
     }
