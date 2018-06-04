f700c1dcda0c7b73c34fea500dfafee67d855267
DESKTOP-0HNVS4D\DucDV <vanducit.98@gmail.com>
2018-06-05 01:58:55 +0700

    remove references unused

--- Project/Data/Infrastructure/Helper.cs
+++ Project/Data/Infrastructure/Helper.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Data.Dtos;
 using Data.Model;
 
@@ -14,18 +11,18 @@ namespace Data.Infrastructure
         {
         }
 
-        public static List<T> GetMasterData<T>(string key1, string key2 = "", string key3 = "") where T : MasterDataDto, new()
+        public static List<MasterDataDto> GetMasterData(string key1, string key2 = "", string key3 = "")
         {
             using (var dbContext = new XeNangEntities())
             {
-                var result = (from c in dbContext.MasterDatas
-                              where c.Keys == key1 || c.Keys == key2 || c.Keys == key3
-                              select new T()
+                var result = (from m in dbContext.MasterDatas
+                              where m.Keys.Equals(key1) || m.Keys.Equals(key2) || m.Keys.Equals(key3)
+                              select new MasterDataDto()
                               {
-                                  ID = c.ID,
-                                  Name = c.Name,
-                                  Value = c.Value
-                              }).ToList<T>();
+                                  Name = m.Name,
+                                  Value = m.Value,
+                                  Key = m.Keys
+                              }).ToList<MasterDataDto>();
                 return result;
             }
         }
