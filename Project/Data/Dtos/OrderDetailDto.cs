b535390f2b16f9ffc1f71ef3ea47c9f85560ff6c
DESKTOP-0HNVS4D\DucDV <vanducit.98@gmail.com>
2018-06-05 02:10:39 +0700

    add OrderDetailDto + create SellRepository (sell and order product)

---
+++ Project/Data/Dtos/OrderDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Data.Dtos
+{
+    public class OrderDetailDto
+    {
+        public int ID { get; set; }
+        public DateTime DateCreate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddr { get; set; }
+        public string CustomerPhoneNo { get; set; }
+        public string CustomerEmail { get; set; }
+        public string ProductName { get; set; }
+        public DateTime DateOfDelivery { get; set; }
+        public int Quantities { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Project/Services/Abstract/SellService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Dtos;
+using Data.Repositories;
+
+namespace Services.Abstract
+{
+    public class SellService : ISellService
+    {
+        private ISellRepo _sellRepo;
+
+        public SellService()
+        {
+            _sellRepo = new SellRepo();
+        }
+
+        public void SellProduct(ProductDto product, int count, DateTime dateCreate)
+        {
+            _sellRepo.SellProduct(product, count, dateCreate);
+        }
+
+        public string OrderProduct(OrderDetailDto order)
+        {
+            return _sellRepo.OrderProduct(order, TODO);
+        }
+
+        public List<OrderDetailDto> GetAllOrdersAreWaiting()
+        {
+            return _sellRepo.GetAllOrdersAreWaiting();
+        }
+    }
+}
